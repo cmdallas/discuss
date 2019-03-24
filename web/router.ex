af84481c767fa08471b5020fe25047dad5181606
cmdallas <self@chrisdallas.tech>
2019-03-24 14:13:23 -0700

    Update `scope "/", Discuss` to use the `resource` handler

--- web/router.ex
+++ web/router.ex
@@ -17,13 +17,7 @@ defmodule Discuss.Router do
     # Use the default browser stack
     pipe_through(:browser)
 
-    get("/", TopicController, :index)
-    get("/topics/new", TopicController, :new)
-    get("/topics/:id/edit", TopicController, :edit)
-
-    post("/topics", TopicController, :create)
-
-    put("/topics/:id", TopicController, :update)
+    resource("/", TopicController)
   end
 
   # Other scopes may use custom stacks.
