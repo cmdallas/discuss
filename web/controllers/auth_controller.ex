6eec0c179cffd2d6b3806bbf58e575de91289337
cmdallas <self@chrisdallas.tech>
2019-03-24 18:49:49 -0700

    🔒 Configure Ueberauth

---
+++ web/controllers/auth_controller.ex
@@ -0,0 +1,11 @@
+defmodule Discuss.AuthController do
+  use Discuss.Web, :controller
+  plug(Ueberauth)
+
+  def callback(conn, params) do
+    IO.puts("+++++++")
+    IO.inspect(conn.assigns)
+    IO.inspect(params)
+    IO.puts("+++++++")
+  end
+end
