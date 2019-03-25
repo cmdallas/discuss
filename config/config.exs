73cdf499a5792823b25e46bba521bb4cdd42574a
cmdallas <self@chrisdallas.tech>
2019-03-24 19:16:20 -0700

    👪 Users migration

--- config/config.exs
+++ config/config.exs
@@ -14,8 +14,7 @@ config :discuss, Discuss.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "IcGzwmn/XbSdnSEY7BAb6ed1aFgFv4KI1hZ+zhgwDb8ibniMl6mJtWFYwFPZZDzG",
   render_errors: [view: Discuss.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Discuss.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: Discuss.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -24,13 +23,13 @@ config :logger, :console,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
 
 config :ueberauth, Ueberauth,
   providers: [
     github: {Ueberauth.Strategy.Github, []}
   ]
 
-config :ueberauth, Ueberauth.Strategy.Github.Oauth,
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
   client_id: System.get_env("GITHUB_OAUTH_CLIENT"),
   client_secret: System.get_env("GITHUB_OAUTH_SECRET")
