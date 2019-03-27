d2437ef18356967768ca348c1134ebf90667efdb
cmdallas <self@chrisdallas.tech>
2019-03-26 22:20:58 -0700

    📨 Write incoming data on the comments channel to the db

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,10 +1,29 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
 
-  def join(name, _params, socket) do
-    {:ok, %{}, socket}
+  alias Discuss.{Comment, Topic}
+
+  def join("comments:" <> topic_id, _params, socket) do
+    topic_id = String.to_integer(topic_id)
+    topic = Repo.get(Topic, topic_id)
+
+    {:ok, %{}, assign(socket, :topic, topic)}
   end
 
-  def handle_in() do
+  def handle_in(name, %{"content" => content}, socket) do
+    topic = socket.assigns.topic
+
+    changeset =
+      topic
+      |> build_assoc(:comments)
+      |> Comment.changeset(%{content: content})
+
+    case Repo.insert(changeset) do
+      {:ok, comment} ->
+        {:reply, :ok, socket}
+
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 end
--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -53,15 +53,22 @@ let socket = new Socket("/socket", { params: { token: window.userToken } });
 
 socket.connect();
 
-// Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("comments:1", {});
-channel
-  .join()
-  .receive("ok", resp => {
-    console.log("Joined successfully", resp);
-  })
-  .receive("error", resp => {
-    console.log("Unable to join", resp);
-  });
+const createSocket = topicId => {
+  let channel = socket.channel(`comments:${topicId}`, {});
+  channel
+    .join()
+    .receive("ok", resp => {
+      console.log("Joined successfully", resp);
+    })
+    .receive("error", resp => {
+      console.log("Unable to join", resp);
+    });
 
-export default socket;
+  document.querySelector("button").addEventListener("click", () => {
+    const content = document.querySelector("textarea").value;
+
+    channel.push("comment:add", { content });
+  });
+};
+
+window.createSocket = createSocket;
